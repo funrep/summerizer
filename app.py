11694d6dd44e3da483d872988618ff1634ecf68c
alexandertu <alexander.tuoma@gmail.com>
2018-04-14 14:15:19 +0200

    Extract header

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return Response("Hello", status=200, mimetype="text/plain")
 
+@app.route('/extract-header', methods=["POST"])
+def extract_header():
+    return request.data[0: min(100, len(request.data))]
+
 if __name__ == '__main__':
     app.run(debug=True)
