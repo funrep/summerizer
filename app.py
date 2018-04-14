4b329b93de46bf9c63054cde71677cb2a067120c
alexandertu <alexander.tuoma@gmail.com>
2018-04-14 16:47:20 +0200

    Route for extracting header

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, Response
+from flask import request
+from flask import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return Response("Hello", status=200, mimetype="text/plain")
+	return Response("Hello", status=200, mimetype="text/plain")
 
-@app.route('/extract-header', methods=["POST"])
+@app.route('/extract-header')
 def extract_header():
-    return request.data[0: min(100, len(request.data))]
+	return request.data[0: min(100, len(request.data))]
 
 if __name__ == '__main__':
     app.run(debug=True)
